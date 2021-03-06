30a672777c6f564f31935e06a32c0df734716c83
MelvinMelen <melen.melvin@gmail.com>
2021-03-06 19:28:03 +0100

    Todo lista, css och js (lägg till i listan)

---
+++ js/code.js
@@ -0,0 +1,35 @@
+//Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList= document.querySelector('.todo-list');
+
+//Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+//Functions
+function addTodo(event) { 
+     //Prevent form from submiting
+    event.preventDefault();
+    //Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    //Create li
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //Completed button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>'
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    //Trash button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+    //Append to list (ul)
+    todoList.appendChild(todoDiv);
+    //Clear todo input value
+    todoInput.value = "";
+}
